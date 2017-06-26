7e1d1485b686effe8a92b4c8cb760a17131c9189
Michael <Michaelye1988@gmail.com>
2017-06-26 23:00:10 +0800

    first commit.

---
+++ app/src/main/java/com/michael/common/MainActivity.java
@@ -0,0 +1,18 @@
+package com.michael.common;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 整理常用的一些组件，方便代码复用，查找
+ */
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
